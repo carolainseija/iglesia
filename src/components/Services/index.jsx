22c2dc034739d67c932aac8dcff0f36f4ec14b5a
carolainseija <carolainsilva1@gmail.com>
2024-02-26 15:23:44 -0300

    services firebase

--- src/components/Services/index.jsx
+++ src/components/Services/index.jsx
@@ -1,31 +1,38 @@
-import service1 from "../../assets/service1.jpg"
-import service2 from "../../assets/service2.jpg"
-import service6 from "../../assets/service6.jpg"
+import { useEffect, useState } from "react"
+import { db } from "../../firebase"
+import { collection, getDocs, query } from "firebase/firestore"
 
-export function Services(){
-    return (
-        <div id="services" className='services'>
-        <p className='subtitle'>¿Que actividades tenemos?</p>
-        <p className='subtitle2'>ERES BIENVENIDO A CADA UNO DE NUESTROS SERVICIOS</p>
-        <div className='informationCards'>
-          <div className='informations'>
-            <div>
-              <img src={service1} alt="Imagen 3" />
-              <h2>Lectura Biblica</h2>
-              <p>Explora nuestros servicios de lectura biblica.</p>
+export function Services() {
+
+  const [services, setServices] = useState([])
+
+  const getServices = async () => {
+    getDocs(query(collection(db, "services")))
+      .then((doc) =>
+        setServices(doc.docs.map((d) => d.data())))
+  }
+
+  useEffect(() => {
+    getServices()
+  }, [])
+
+  return (
+    <div id="services" className='services'>
+      <p className='subtitle'>¿Que actividades tenemos?</p>
+      <p className='subtitle2'>ERES BIENVENIDO A CADA UNO DE NUESTROS SERVICIOS</p>
+      <div className='informationCards'>
+          {services.length > 0 && (
+            <div className='informations'>
+              {services.map((service) => (
+                <div>
+                  <img src={service.image} alt="Imagen 3" />
+                  <h2>{service.title}</h2>
+                  <p>{service.description}</p>
+                </div>
+              ))}
             </div>
-            <div>
-              <img src={service2} alt="Imagen 3" />
-              <h2>Oración</h2>
-              <p>Explora nuestros servicios y tiempo de oraciòn.</p>
-            </div>
-            <div>
-              <img src={service6} alt="Imagen 3" />
-              <h2>Reuniones</h2>
-              <p>Explora nuestros servicios de adoración y descubre cómo puedes unirte a nosotros en la celebración.</p>
-            </div>
-          </div>
-        </div>
+          )}
       </div>
-    )
+    </div>
+  )
 }
