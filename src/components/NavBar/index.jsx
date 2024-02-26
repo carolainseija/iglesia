129662d37a542b60ef9cf25d558cbe540799fb79
carolainseija <carolainsilva1@gmail.com>
2024-02-26 19:25:28 -0300

    nav bar

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import './style.css'
+
 
 export function NavBar() {
 
--- src/components/NavbarPages/index.jsx
+++ src/components/NavbarPages/index.jsx
@@ -10,7 +10,7 @@ export function NavBarPages() {
     return (
         <div className="navbarPages">
             <div> <img className="logo" src="https://idpmisi.com/wp-content/uploads/2021/08/favicon.ico.png" alt="logo de la iglesia" /></div>
-            <div className={`menu ${showMenu ? 'show' : ''}`}>
+            <div className={`menu-sm ${showMenu ? 'show-sm' : ''}`}>
                 <a href="/">Inicio</a>
                 <a href="/#about">Nosotros</a>
                 <a href="/#services">Servicio</a>
@@ -18,11 +18,11 @@ export function NavBarPages() {
                 <a href="/proyecto">Proyecto</a>
                 <a href="/#footer">Contacto</a>
             </div>
-            <div className="hamburger-menu" onClick={toggleMenu}>
-                <div className="bar"></div>
-                <div className="bar"></div>
-                <div className="bar"></div>
-                <div className="bar"></div>
+            <div className="hamburger-menu-sm" onClick={toggleMenu}>
+                <div className="bar-sm"></div>
+                <div className="bar-sm"></div>
+                <div className="bar-sm"></div>
+                <div className="bar-sm"></div>
             </div>
         </div>
     )
