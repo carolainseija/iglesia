fad4905c720154ada054b5a9a00aa65baa48ec68
carolainseija <carolainsilva1@gmail.com>
2024-02-26 17:17:17 -0300

    gallery

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,47 +1,47 @@
-import { GallerySwiper } from "./components/GallerySwiper";
-import audiovisual from '../src/assets/audiovisual1.jpg'
-
-import esc1 from '../src/assets/esc1.jpg'
-import esc2 from '../src/assets/esc2.avif'
-import esc3 from '../src/assets/esc3.jpg'
-import esc4 from '../src/assets/esc4.jpg'
-import esc5 from '../src/assets/esc5.webp'
 import { Footer } from "./components/Footer";
-import { NavBar } from "./components/NavBar";
 import { NavBarPages } from "./components/NavbarPages";
+import { collection, getDocs, query } from "firebase/firestore";
+import { db } from "./firebase";
+import { useEffect, useState } from "react";
 
 
-const GalleryItem = ({ imageUrl, altText }) => {
+const GalleryItem = ({ imageUrl, key }) => {
   return (
     <div className="gallery-item">
-      <img  src={imageUrl} alt={altText} />
+      <img key={key}  src={imageUrl} />
     </div>
   );
 };
 
 export default function Gallery() {
-  const images = [
-    { id: 1, url: esc1, alt: 'Image 1' },
-    { id: 2, url: esc2, alt: 'Image 2' },
-    { id: 3, url: esc3, alt: 'Image 3' },
-    { id: 4, url: esc4, alt: 'Image 1' },
-    { id: 5, url: esc5,  alt: 'Image 2' },
-    { id: 6, url: esc1, alt: 'Image 3' },
-  ];
+
+  const [images, setImages] = useState([])
+
+  const getGallery = async () => {
+    getDocs(query(collection(db, "gallery")))
+      .then((doc) =>
+        setImages(doc.docs.map((d) => ({
+          id: d.id,
+          ...d.data()
+        }))))
+  }
+
+  useEffect(() => {
+    getGallery()
+  }, [])
+
   return (
     <>
     <NavBarPages />
       <div className="headerGallery">
       </div>
       <div className="galleryPage">
-        <h2>      Nuestra Galeria de fotos
-        </h2>
+        <h2>Nuestra Galeria de fotos</h2>
         <div className="gallery-container">
-          {images.map((image) => (
-            <GalleryItem key={image.id} imageUrl={image.url} altText={image.alt} />
-          ))}
+           {images.map((image) => (
+            <GalleryItem key={image.id} imageUrl={image.href}  />
+          ))} 
         </div>
-        <GallerySwiper />
       </div>
       <Footer />
       </>
